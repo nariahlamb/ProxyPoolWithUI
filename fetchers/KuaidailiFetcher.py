71bc9f4617752a7e370f2062ad9bfd6f9151adb5
Arronlong <>
2025-05-21 15:51:55 +0800

    .

--- fetchers/KuaidailiFetcher.py
+++ fetchers/KuaidailiFetcher.py
@@ -3,6 +3,8 @@
 from .BaseFetcher import BaseFetcher
 import requests
 from pyquery import PyQuery as pq
+import re
+import json
 
 class KuaidailiFetcher(BaseFetcher):
     """
@@ -15,18 +17,31 @@ class KuaidailiFetcher(BaseFetcher):
         返回示例：[('http', '127.0.0.1', 8080), ('http', '127.0.0.1', 1234)]
         """
         
-        urls = []
-        urls = urls + [f'https://www.kuaidaili.com/free/fps/{page}/' for page in range(1, 11)]
+        urls = [f'https://www.kuaidaili.com/free/fps/{page}/' for page in range(1, 11)]
+
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Upgrade-Insecure-Requests': '1',
+            'Cookie': '__tst_status=483636310#; EO_Bot_Ssid=2094989312; channelid=0;',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.130 Chrome/79.0.3945.130 Safari/537.36'
+        }
+
 
         proxies = []
 
         for url in urls:
-            html = requests.get(url, timeout=10).text
-            doc = pq(html)
-            for item in doc('table tbody tr').items():
-                ip = item.find('td[data-title="IP"]').text()
-                port = int(item.find('td[data-title="PORT"]').text())
-                proxies.append(('http', ip, port))
+            html = requests.get(url, timeout=10, headers=headers).text
+            match = re.search(r'const\s+fpsList\s*=\s*(.*?\]);', html, re.DOTALL)
+            if match:
+                fps_list_str = match.group(1)
+                fps_list = json.loads(fps_list_str)
+                result = [('http', item['ip'], item['port']) for item in fps_list]
+                proxies.extend(result)
         
         proxies = list(set(proxies))
 
